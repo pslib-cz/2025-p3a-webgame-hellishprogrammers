5941742cbade8fc27855865ce05b19ac462bb528
DanielBrzezina <daniel.brzezina.023@pslib.cz>
2025-12-09 21:20:51 +0100

    IconButton

---
+++ synergydistrict.client/src/components/IconButton.tsx
@@ -0,0 +1,18 @@
+import type { FC } from "react";
+import IconButtonStyle from "../styles/IconButton.module.css"
+type IconButtonProps = {
+    isSelected: boolean,
+    iconKey:string,
+    OnClick:() => void,
+};
+
+export const IconButton:FC<IconButtonProps> = ({isSelected,iconKey,OnClick}) => {
+    const handleOnClick = () =>{
+        OnClick()
+    }
+    return(
+        <button onClick={handleOnClick} className={isSelected? IconButtonStyle.selected:IconButtonStyle.default}>
+            iconKey
+        </button>
+    )
+}
