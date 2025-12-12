7356ac4d0242e245321c8ef35c8b85b1c23ed39a
Mykhailo-Odynets <mykhailo.odynets.023@pslib.cz>
2025-12-12 20:25:29 +0100

    close icon design

--- synergydistrict.client/src/pages/Menu/PlayMenu.tsx
+++ synergydistrict.client/src/pages/Menu/PlayMenu.tsx
@@ -1,11 +1,9 @@
 import TextButton from "../../components/TextButton";
-import "../../styles/icons.css";
 
 const PlayMenu = () => {
   return (
     <>
       <TextButton text="game" linkTo="/game" />
-      <p className="icon">forest</p>
     </>
   );
 };
