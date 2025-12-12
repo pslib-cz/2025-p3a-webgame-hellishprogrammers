a53dc9bd258132f68d5d5826ef2834eb8c697d5c
Mykhailo-Odynets <mykhailo.odynets.023@pslib.cz>
2025-12-12 18:30:05 +0100

    icons fix

--- synergydistrict.client/src/pages/Menu/PlayMenu.tsx
+++ synergydistrict.client/src/pages/Menu/PlayMenu.tsx
@@ -1,11 +1,12 @@
-import { IconClose } from "../../components/Icons";
+import { IconClose, IconHappiness, IconMountain } from "../../components/Icons";
 import TextButton from "../../components/TextButton";
+import "../../styles/icons.css";
 
 const PlayMenu = () => {
   return (
     <>
       <TextButton text="game" linkTo="/game" />
-      <IconClose />
+      <IconMountain />
     </>
   );
 };
