29ebbec0f27449c736d2e79be7f5674113fea5bb
DanielBrzezina <daniel.brzezina.023@pslib.cz>
2025-12-15 20:17:22 +0100

    buildingbutton and IconButtonchange

--- synergydistrict.client/src/components/BuildingButton.tsx
+++ synergydistrict.client/src/components/BuildingButton.tsx
@@ -1,12 +1,20 @@
-import { useState } from "react"
-import IconButtonStyle from"../styles/IconButton.css"
-export const BuildingButton = () => {
-    const [value, setValue] = useState();
-    const IsSelected:boolean = false
+import { useState, type FC } from "react"
+import { IconButton } from "./IconButton"
+type BuildingButtonProps = {
+    amount:number,
+    buildingIconName:string,
+    OnClick:() => void,
+};
+
+export const BuildingButton:FC<BuildingButtonProps> = (props:BuildingButtonProps) => {
+    const handleOnClick = (inString:string) => {
+        setActive(inString)
+        props.OnClick()
+    }
+    const [active,setActive] = useState<string>();
     return(
-        <button onClick={}>
-            <span>obrazek</span>
-            <span>1</span>
-        </button>
+        
+        <IconButton OnClick={() => handleOnClick(props.buildingIconName)} isSelected={active == props.buildingIconName } iconKey={props.buildingIconName} amount={props.amount}></IconButton>
+        
     )
 }
