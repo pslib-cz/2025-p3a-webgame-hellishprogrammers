e2ecffe2c2bf5a7d2ada90c2043ec5c7af7f5a34
Matej-Kovar <matej.kovar.023@pslib.cz>
2026-01-28 20:27:03 +0100

    completed left panel

--- synergydistrict.client/src/pages/Game/BuildingDocs/BuildingDocs.tsx
+++ synergydistrict.client/src/pages/Game/BuildingDocs/BuildingDocs.tsx
@@ -1,4 +1,4 @@
-import { type FC, useEffect, useRef } from "react";
+import { type FC, useEffect, useRef, useState } from "react";
 import ProductionListing from "../../../components/Game/ProductionListing/ProductionListing";
 import ValuesBox from "../../../components/Game/ValuesBox/ValuesBox";
 import ShowInfo from "../../../components/ShowInfo/ShowInfo";
@@ -6,6 +6,12 @@ import type { BuildingType } from "../../../types/Game/Buildings";
 import styles from "./BuildingDocs.module.css";
 import { useBuildingsBitmap } from "../../../hooks/providers/useBuildingsBitmap";
 import TextButton from "../../../components/Buttons/TextButton/TextButton";
+import type { BuildingCategory } from "../../../types/";
+import ToggleButton from "../../../components/Buttons/ToggleButton/ToggleButton";
+import { useGameData } from "../../../hooks/providers/useGameData";
+import SynergyDisplay from "../SynergyDisplay";
+
+const buildingCategories: BuildingCategory[] = ["Residential", "Commercial", "Industrial", "Extractional", "Recreational"];
 
 type BuildingDocsProps = {
     building: BuildingType;
@@ -14,7 +20,9 @@ type BuildingDocsProps = {
 const BuildingDocs: FC<BuildingDocsProps> = ({ building }) => {
     const { buildingsBitmap } = useBuildingsBitmap();
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
-    //const [currnetButton,setCurrnetButton] = useState<BuildingCategory>("Residential")
+    const [selectedCategory, setSelectedCategory] = useState<BuildingCategory>("Residential");
+    const { synergies, buildings } = useGameData()
+    const [IO,setIO] = useState<boolean>(false)
     useEffect(() => {
         const canvas = canvasRef.current!;
         const context = canvas?.getContext("2d");
@@ -32,7 +40,7 @@ const BuildingDocs: FC<BuildingDocsProps> = ({ building }) => {
     }, [canvasRef, building]);
 
     return (
-        <div className={styles.buildingDocs}>
+        <div className={styles.buildingDocs} style={{userSelect: "none"}}>
             <div className={styles.title}>
                 <h2>{building.name.toUpperCase()}_</h2>
                 <span className={`${styles.icon} icon`}>{building.iconKey}</span>
@@ -56,12 +64,44 @@ const BuildingDocs: FC<BuildingDocsProps> = ({ building }) => {
                 ))}
             </div>
             <ProductionListing title="Shape">
-                <canvas ref={canvasRef} style={{width: "100%"}}/>
+                <canvas ref={canvasRef} style={{ width: "100%" }} />
             </ProductionListing>
-            <div>
-                <h2 className={styles.title}>Synergy</h2>
-                <TextButton  isActive={true}  text="Residential" bacgroundColor="--primary"></TextButton>
-                
+            <div style={{display: "flex", flexDirection: "column", gap: ".5rem"}}>
+                <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+                    <h2 className={styles.title}>Synergy</h2>
+                    <div style={{ fontSize: "0.75rem"}}>
+                        <ToggleButton options={["I","O"]} onChange={() => setIO(io => !io)}/>
+                    </div>
+                </div>
+
+                <div style={{display: "flex", flexDirection: "column"}}>
+                    {
+                        buildingCategories.map((category) => (
+                            <TextButton key={category} isActive={selectedCategory === category} text={category} bacgroundColor={`--${category.toLowerCase()}`} textAlign="left" onClick={() => setSelectedCategory(category)}></TextButton>
+                        ))
+                    }
+                </div>
+                <div style={{display: "flex", flexDirection: "column", gap: ".5rem"}}>
+                    {
+                        synergies.filter(s => 
+                        {
+                            const other = buildings.find(b => b.buildingId == (IO ? s.sourceBuildingId : s.targetBuildingId))
+                            if(IO){
+                                return s.targetBuildingId == building.buildingId && other?.type == selectedCategory
+                            }
+                            else
+                            {
+                                return s.sourceBuildingId == building.buildingId && other?.type == selectedCategory
+                            }
+                        }).map(s => 
+                            { 
+                                const other = buildings.find(b => b.buildingId == (IO ? s.sourceBuildingId : s.targetBuildingId))
+                                if(!other) return;
+                                return <SynergyDisplay id={IO ? s.sourceBuildingId.toString() : s.targetBuildingId.toString()} name={other?.name} amount={null} productions={s.synergyProductions}/> 
+                            } 
+                        )
+                    }
+                </div>
             </div>
         </div>
     );
