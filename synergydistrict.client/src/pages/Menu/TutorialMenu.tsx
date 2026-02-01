1c46468ef91f3a5eea7d52d19a9b28bbf0b4e65b
DanielBrzezina <daniel.brzezina.023@pslib.cz>
2026-02-01 18:20:52 +0100

    tutorial des

--- synergydistrict.client/src/pages/Menu/TutorialMenu.tsx
+++ synergydistrict.client/src/pages/Menu/TutorialMenu.tsx
@@ -1,7 +1,20 @@
+import { useRef } from "react";
+import TextButton from "../../components/Buttons/TextButton/TextButton";
 import Construction from "../../components/Construction";
 
+
 const TutorialMenu = () => {
-    return <Construction />;
+    return (
+    <>
+    <div>
+    <h2>How to play SYNERGY DISTRICT</h2>
+    <p>Synergy District is a grid based game 
+        where you place buildings that have synergyes with eachother and the map.
+        Each building is of a certain type like this</p>
+    </div>
+    </>
+    );
+    
 };
 
 export default TutorialMenu;
