85244d6c0240c5e00552c6acd49c5426df7363a5
Mykhailo-Odynets <mykhailo.odynets.023@pslib.cz>
2026-02-05 19:33:58 +0100

    ez icons fix

--- synergydistrict.client/src/components/Icons.tsx
+++ synergydistrict.client/src/components/Icons.tsx
@@ -1,8 +1,11 @@
 import "../styles/icons.css";
 
 export const GetIcon = (ligature: string) => {
-    return <span translate="no" style={{fontFamily: "icons"}}>{ligature}</span>
-}
+    return (
+        <span translate="no" className="icon">
+            {ligature}
+        </span>
+    );
+};
 
 export const IconClose = () => GetIcon("close");
-
